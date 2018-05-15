9406e2fb57817bf3184668be27b6b530ea4fe67e
ohrodion <rodi_ocean@icloud.com>
2018-05-16 01:06:24 +0300

    justfortest

--- routes/licApi.js
+++ routes/licApi.js
@@ -9,7 +9,7 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "http://justfortest.gq");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
     next();
